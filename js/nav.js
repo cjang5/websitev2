2b5d58248d7f9fa20c1087fc5f199ab240cdbc0e
cjang5 <chrismangggg@gmail.com>
2015-05-18 20:28:37 -0400

    Added pseudo navbar made up of 3 buttons

---
+++ js/nav.js
@@ -0,0 +1,19 @@
+// Initialize pseudo-navbar's wrapper so elements below don't jump upon fixing it to the top
+$(".pseudo-nav-wrapper").height($(".pseudo-nav").height());
+
+var tn = $(".pseudo-nav");
+var tns = "test-nav-scrolled";
+var height = $(".pseudo-nav").position().top - 50;
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > height) {
+        tn.addClass(tns);
+        $(".pseudo-nav-wrapper").addClass("wrapper-adjust");
+        $(".nav-bg").addClass("bg-white");
+    } else {
+        tn.removeClass(tns);
+        $(".pseudo-nav-wrapper").removeClass("wrapper-adjust");
+        $(".nav-bg").removeClass("bg-white");
+    }
+});
+    
